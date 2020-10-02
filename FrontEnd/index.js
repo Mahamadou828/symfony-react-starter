732399f09883e12e50b7b88db0ed50c746d2fc93
Mamadou <opmadou@gmail.com>
2020-10-02 18:09:01 +0300

    first commit

---
+++ FrontEnd/index.js
@@ -0,0 +1,5 @@
+import ReactDOM from "react-dom";
+import React from "react";
+import App from "./components/App";
+console.log("couccou je us");
+ReactDOM.render(<App />, document.querySelector("#app"));
